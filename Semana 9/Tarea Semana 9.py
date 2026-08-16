eafa07db613e1939c63e484bd162311d186a7ab7
Liliam Illanes <la.illanesb@uea.edu.ec>
2026-08-15 19:21:20 -0500

    Creación de la tarea de la semana 9 Arreglo simple + instalación de Git

---
+++ Semana 9/Tarea Semana 9.py
@@ -0,0 +1,12 @@
+# Tarea Semana 9 - Arreglo simple
+
+# 1. Crear el arreglo con los cinco valores (10, 20, 30, 40, 50)
+numeros = [10, 20, 30, 40, 50]
+
+# 2. Mostrar el valor de la POSICION 1
+print("El numero de la posicion 1 es:", numeros [1])
+
+# 3. Recorrer todo el arreglo e imprimir cada elemento
+print("Todos los elementos del arreglo:")
+for numero in numeros:
+    print(numero)
