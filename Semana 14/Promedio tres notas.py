4bf45ffeb3d80b090ecf4ed81e5d19342cc4f533
Liliam Illanes <la.illanesb@uea.edu.ec>
2026-09-20 17:26:34 -0500

    Creación de la tarea de la semana 14 Tarea: Programación Básica Funciones con parámetros y retorno de valores El programa debe incluir obligatoriamente:
    Una función
    Parámetros de entrada
    Uso del comando/palabra clave return
    Una llamada a la función
    Mostrar el resultado en pantalla

---
+++ Semana 14/Promedio tres notas.py
@@ -0,0 +1,19 @@
+"""Programa para calcular el promedio de tres notas."""
+
+def calcular_promedio(nota1: float, nota2: float,  nota3: float) -> float:
+    """Calcula el promedio de tres notas y lo devuelve"""
+    promedio = (nota1 + nota2 + nota3) / 3
+    return promedio
+
+if __name__ == "__main__":
+    try:
+        nota1 = float(input("Ingrese la nota: "))
+        nota2 = float(input("Ingrese la nota: "))
+        nota3 = float(input("Ingrese la nota: "))
+
+    except EOFError:
+            nota1, nota2, nota3 = 0, 0, 0
+    resultado = calcular_promedio(nota1, nota2, nota3)
+    print(f"El promedio de las tres notas es: {resultado:.2f}")
+
+
